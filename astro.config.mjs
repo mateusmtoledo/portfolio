d5f0641cd89db1f8bf91ac8d84cc527b3cd85af6
Mateus Toledo <mateusmtoledo1@gmail.com>
2023-02-23 21:03:06 -0300

    Add pt-br translation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), astroI18next()],
 });
