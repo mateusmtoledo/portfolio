26f1601f75cf19017c71a1b7a32497a8a184f9bc
Mateus Toledo <mateusmtoledo1@gmail.com>
2023-02-28 05:23:47 -0300

    Add translation to notifications

--- src/utils/sendNotification.ts
+++ src/utils/sendNotification.ts
@@ -1,58 +1,64 @@
-function successNotification(text: string) {
+function successNotification(text: string, title: string = "Success") {
   const notification = document.createElement("div");
   notification.classList.add(
     "bg-green-100",
     "border-l-4",
     "border-green-500",
     "text-green-700",
-    "p-4"
+    "p-4",
+    "w-full"
   );
 
-  const title = document.createElement("p");
-  title.classList.add("font-bold");
-  title.textContent = "Success";
+  const toastTitle = document.createElement("p");
+  toastTitle.classList.add("font-bold");
+  toastTitle.textContent = title;
 
   const p = document.createElement("p");
   p.textContent = text;
 
-  notification.append(title, p);
+  notification.append(toastTitle, p);
 
   return notification;
 }
 
-function errorNotification(text: string) {
+function errorNotification(text: string, title: string = "Error") {
   const notification = document.createElement("div");
   notification.classList.add(
     "bg-red-100",
     "border-l-4",
     "border-red-500",
     "text-red-700",
-    "p-4"
+    "p-4",
+    "w-full"
   );
 
-  const title = document.createElement("p");
-  title.classList.add("font-bold");
-  title.textContent = "Error";
+  const toastTitle = document.createElement("p");
+  toastTitle.classList.add("font-bold");
+  toastTitle.textContent = title;
 
   const p = document.createElement("p");
   p.textContent = text;
 
-  notification.append(title, p);
+  notification.append(toastTitle, p);
 
   return notification;
 }
 
-function generateNotification(type: string, text: string) {
+function generateNotification(type: string, text: string, title?: string) {
   switch (type) {
     case "success":
-      return successNotification(text);
+      return successNotification(text, title);
     default:
-      return errorNotification(text);
+      return errorNotification(text, title);
   }
 }
 
-export default function sendNotification(type: string, text: string) {
-  const notification = generateNotification(type, text);
+export default function sendNotification(
+  type: string,
+  text: string,
+  title?: string
+) {
+  const notification = generateNotification(type, text, title);
 
   const container = document.getElementById("toast");
   if (!container) throw new Error("Toast container not found");
